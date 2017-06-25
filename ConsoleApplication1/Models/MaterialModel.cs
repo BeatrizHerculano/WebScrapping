094bea90fce1972744f3621965b2b6d3a603de03
beatrizher <biaher.oliveira@gmail.com>
2017-06-25 12:53:16 -0300

    initial commit

---
+++ ConsoleApplication1/Models/MaterialModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapping.Models
+{
+    class MaterialModel
+    {
+        public string verbo;
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WebScrapping.Models;
+using System.Text;
+
+namespace WebScrapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> verbosInfinitivos = new List<string>();
+                verbosInfinitivos.Add("olho");
+                verbosInfinitivos.Add("caio");
+                verbosInfinitivos.Add("sei");
+                verbosInfinitivos.Add("corre");
+                verbosInfinitivos.Add("vimos");
+                verbosInfinitivos.Add("gosto");
+                verbosInfinitivos.Add("Deus");  
+                var baseUrl = "https://www.conjugacao.com.br";
+                var client = new HtmlWeb();
+                client.OverrideEncoding = Encoding.UTF8;
+                foreach (string verbo in verbosInfinitivos)
+                {
+                    var conjugations = client.Load(baseUrl + "/busca.php?q=" + verbo);
+                    if (conjugations.DocumentNode.SelectNodes("//span[@class=\"f\"]") != null)
+                    {
+                        var conjugationLength = conjugations.DocumentNode.SelectNodes("//span[@class=\"f\"]").Count;
+                        var conjugationList = conjugations.DocumentNode.SelectNodes("//span[@class=\"f\"]");
+                        //
+                        Console.WriteLine(conjugationLength);
+                        if (conjugationLength > 0)
+                        {
+                            Console.WriteLine(conjugationList[0].InnerText);
+                            //foreach (var item in conjugationList)
+                            //{
+                            //    Console.WriteLine(item.InnerHtml);
+                            //}
+                            Console.WriteLine("");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("O verbo " + verbo + " nao se encontra na base utilizada ou nao tem um infinitivo.");
+                    }                    
+                }
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
